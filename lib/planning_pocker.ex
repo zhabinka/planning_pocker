2d194ebaf49f5c7d25b4813a2e86c28fc1a05780
Sergey Karpuk <sergeykarpuk@yandex.by>
2023-09-18 17:56:50 +0300

    add hello func

---
+++ lib/planning_pocker.ex
@@ -0,0 +1,18 @@
+defmodule PlanningPocker do
+  @moduledoc """
+  Documentation for `PlanningPocker`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PlanningPocker.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
